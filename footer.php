5c74ff971e1655626ae7fdc763f6d40c8cbd9586
Furkan Çeker <ceker0901@gmail.com>
2023-02-08 23:42:49 +0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,96 @@
+
+        <!--Footer__Area-Start-->
+        <footer class="footer__area section__relative">
+            <div class="section__shape">
+                <svg class="svg_element">
+                    <use xlink:href="#svg__element-9" />
+                </svg>
+            </div>
+            <!--Footer_Bottom_Area-Start-->
+            <div class="footer__top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 pr-lg-5">
+                            <div class="footer__widget anim_top">
+                                <h4 class="widget__title">About Us</h4>
+                                <div class="desc mb-4">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, Lorem ipsum dolor sit amet,</p>
+                                </div>
+                                <div class="social__links">
+                                    <a href="#"><i class="ri-facebook-fill"></i></a>
+                                    <a href="#"><i class="ri-twitter-fill"></i></a>
+                                    <a href="#"><i class="ri-instagram-fill"></i></a>
+                                    <a href="#"><i class="ri-linkedin-fill"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-2">
+                            <div class="footer__widget anim_top">
+                                <h4 class="widget__title">Explore</h4>
+                                <ul class="nav-link">
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Testimonial</a></li>
+                                    <li><a href="#">Expert Team</a></li>
+                                    <li><a href="#">Posts</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-lg-2">
+                            <div class="footer__widget anim_top">
+                                <h4 class="widget__title">Support</h4>
+                                <ul class="nav-link">
+                                    <li><a href="#">Pet Shower</a></li>
+                                    <li><a href="#">Health checkup</a></li>
+                                    <li><a href="#">Pet Grooming</a></li>
+                                    <li><a href="#">Spa and Grooming</a></li>
+                                    <li><a href="#">Brush &amp; Blow Dry</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Footer_Bottom_Area-End-->
+            <!--Footer_Top_Area-Start-->
+            <div class="footer__bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="copyright text-center mb-3 mb-md-0">Copyright &copy; 2023. All Rights Reserved</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Footer_Top_Area-End-->
+        </footer>
+        <!--Footer__Area-End-->
+    </main>
+    <!-- Full-Wrapper-End -->
+    <!-- Progress_Scroll-To-Top -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+        <span class="icon"><i class="ri-arrow-up-line"></i></span>
+    </div>
+    <!--=======  SCRIPTS =======-->
+    <script src="js/vendor/jquery-min.js"></script>
+    <!--=======  PLUGINS =======-->
+    <script src="js/bootstrap-min.js"></script>
+    <script src="js/appear.js"></script>
+    <script src="js/ajaxchimp.js"></script>
+    <script src="js/animatenumber-min.js"></script>
+    <script src="js/contact-form.js"></script>
+    <script src="js/imagesloaded-min.js"></script>
+    <script src="js/isotope-pkgd-min.js"></script>
+    <script src="js/lity-min.js"></script>
+    <script src="js/scrollreveal-min.js"></script>
+    <script src="js/swiper-bundle-min.js"></script>
+    <!--=======  ACTIVE JS =======-->
+    <script src="js/main.js"></script>
+</body>
+
+
+<!-- Mirrored from html.devignedge.com/pettu/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 22 Jan 2023 20:48:06 GMT -->
+</html>
---
+++ header.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="zxx">
+<!--<![endif]-->
+
+
+<!-- Mirrored from html.devignedge.com/pettu/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 22 Jan 2023 20:47:32 GMT -->
+<head>
+    <!--====== USEFULL META ======-->
+    <meta charset="utf-8">
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <![endif]-->
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--====== Favicon ======-->
+    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!--====== TITLE TAG ======-->
+    <title>PetDay | Pet Kuaför</title>
+    <!--====== STYLESHEETS ======-->
+    <link rel="stylesheet" href="css/swiper-bundle-min.css" />
+    <link rel="stylesheet" href="css/bootstrap-min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/remixicon.css">
+    <link rel="stylesheet" href="css/theme-icon.css">
+    <link rel="stylesheet" href="css/lity-min.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <!--====== MAIN STYLESHEET ======-->
+    <link href="style.css" rel="stylesheet">
+    <script src="js/vendor/modernizr.js"></script>
+    <!--[if lt IE 9]>
+        <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body data-spy="scroll" data-target=".nav__area" data-offset="50">
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <!-- Preloader-Content -->
+    <div class="preloader">
+        <img src="images/preloader.gif" alt="">
+    </div>
+
+    <!-- nav__area-Start -->
+    <div class="nav__area nav__transparent" data-spy="affix" data-offset-top="197">
+        <div class="container nav__row">
+            <!-- nav__logo-Start -->
+            <div class="nav__logo">
+                <!-- Text-Logo-Markup -->
+                <!--<h2 class="nav__logo-text"><a href="#">Gority</a></h2>-->
+                <!-- Image-Logo-Markup -->
+                <a href="index.html" class="nav__logo-image logo__light"><img src="images/logo-light.png" alt="Gority"></a>
+                <a href="index.html" class="nav__logo-image logo__dark"><img src="images/logo-dark.png" alt="Gority"></a>
+            </div>
+            <!--nav__logo-End-->
+            <div class="nav__toggle" id="nav-toggle">
+                <i class="ri-function-line"></i>
+            </div>
+            <div class="nav__right" id="nav-right">
+                <div class="nav__close" id="nav-close">
+                    <i class="ri-close-line"></i>
+                </div>
+                <!-- nav__menu-Start -->
+                <nav class="nav__menu" id="nav-menu">
+                    <ul class="nav">
+                        <li><a href="index.php">Ana Sayfa</a></li>
+                        <li><a href="hakkimizda.php">Hakkımızda</a></li>
+                        <li><a href="hizmetler.php">Hizmetler</a></li>
+                        <li><a href="services.html">Services</a>
+                            <ul class="sub-menu">
+                                <li><a href="services.html">Services</a></li>
+                                <li><a href="single-service.html">Service Details</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="posts.html">Pages</a>
+                            <ul class="sub-menu">
+                                <li><a href="team.html">Team</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="team.html">Team</a></li>
+                                        <li><a href="single-team.html">Team Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="product.html">Products</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="product.html">Products</a></li>
+                                        <li><a href="single-product.html">Product Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="gallery.html">Gallery</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="posts.html">Posts</a>
+                            <ul class="sub-menu">
+                                <li><a href="posts.html">Posts</a></li>
+                                <li><a href="single-post.html">Post Details</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="iletisim.php">İletişim</a></li>
+                    </ul>
+                </nav>
+                <!-- nav__menu-End -->
+                <div class="nav__dark">
+                    <!-- Theme change button -->
+                    <i class="ri-moon-line change-theme" id="theme-button"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- nav__area-End -->
